100c47560a16fbd189f0817e822c149cde433892
Agnelo Nicolaus Fernandes <agnelofernandes475@gmail.com>
2023-05-17 16:30:29 +0530

    Django HTML 100% Fix

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,13 @@ urlpatterns = [
     # path('404/', views.errorpage, name='404'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('achivements/', views.achivements, name='achivements'),
+    path('blog/', views.blog, name='blog'),
+    path('blogDetails/', views.blogDetails, name='blogDetails'),
+    path('clubHistory/', views.clubHistory, name='clubHistory'),
+
+    #Auth Urls
+    path('login/', views.user_login, name='user_login'),
+    path('clubHistory/', views.user_register, name='user_register'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -20,3 +20,54 @@ def contact(request):
         'nbar': 'contact'
     }
     return render(request, 'contact.html', context)
+
+def achivements(request):
+    context = {
+        'nbar': 'achivement'
+    }
+    return render(request, 'achivement_details.html', context)
+
+def blog(request):
+    context = {
+        'nbar': 'blog'
+    }
+    return render(request, 'blog.html', context)
+
+def blogDetails(request):
+    context = {
+        'nbar': 'blogDetails'
+    }
+    return render(request, 'blogDetails.html', context)
+
+def clubHistory(request):
+    context = {
+        'nbar': 'clubHistory'
+    }
+    return render(request, 'clubHistory.html', context)
+
+def gallery(request):
+    context={
+        'nbar': 'gallery'
+    }
+    return render(request, 'gallery02.html', context)
+
+def playerDetails(request):
+    context={
+        'nbar': 'playerDetails'
+    }
+    return render(request, 'playerDetails.html', context)
+
+def user_login(request):
+    context={
+        'nbar': 'login'
+    }
+    return render(request, 'login.html', context)
+
+def user_register(request):
+    context={
+        'nbar': 'register'
+    }
+    return render(request, 'register.html', context)
+
+
+
